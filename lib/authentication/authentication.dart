d2a38fd6de0432faeae8c1477cff55d7a7f037fd
ZahangirAlamJP <thezahangir@gmail.com>
2022-03-29 20:36:39 +0600

    Complate * Firebase Auth And Login With Phone Number Verification

---
+++ lib/authentication/authentication.dart
@@ -0,0 +1,22 @@
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:zshop/home.dart';
+
+import 'login_screen/login_screen_view.dart';
+
+class Authentication extends StatelessWidget {
+  Authentication({Key? key}) : super(key: key);
+
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context) {
+    if (auth.currentUser != null) {
+      return HomeScreenView();
+    } else {
+      return LoginScreen();
+    }
+  }
+}
